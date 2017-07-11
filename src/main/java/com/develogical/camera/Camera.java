6d4e3b51b736ecdf55cfa8fc4a2cecef8b31d737
Christy Kin-Cleaves <Christy@Kin-Cleaves.com>
2017-07-11 16:27:03 +0100

    Stuff

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -6,22 +6,36 @@ public class Camera {
     private MemoryCard memoryCard;
     private WriteCompleteListener writeCompleteListener;
     private boolean isSensorOn;
+    private boolean busy;
 
+    class WCL implements  WriteCompleteListener{
 
+        @Override
+        public void writeComplete() {
+            writeIsComplete();
+        }
+    }
 
-
-    public Camera(Sensor sensor, MemoryCard memoryCard, WriteCompleteListener writeCompleteListener)
+    public Camera(Sensor sensor, MemoryCard memoryCard)
     {
         this.sensor = sensor;
         this.memoryCard = memoryCard;
-        this.writeCompleteListener = writeCompleteListener;
+        this.writeCompleteListener = new WCL();
         this.isSensorOn = false;
+        this.busy = false;
+    }
+
+    public Camera(Sensor sensor, MemoryCard memoryCard, WriteCompleteListener wcl)
+    {
+        this(sensor, memoryCard);
+        this.writeCompleteListener = wcl;
     }
 
     public void pressShutter() {
         if(this.isSensorOn)
         {
-            this.memoryCard.write(this.sensor.readData(), this.writeCompleteListener);
+            this.memoryCard.write(this.sensor.readData(), () -> busy = false);
+            this.writeInProcess();
         }
     }
 
@@ -32,9 +46,28 @@ public class Camera {
     }
 
     public void powerOff() {
-        //Turn off sensor
-        this.sensor.powerDown();
-        this.isSensorOn = false;
+        if(!this.isBusy())
+        {
+            //Turn off sensor
+            this.sensor.powerDown();
+            this.isSensorOn = false;
+        }
+
+    }
+
+    private boolean isBusy()
+    {
+        return this.busy;
+    }
+
+    public void writeIsComplete()
+    {
+        this.busy = false;
+    }
+
+    private void writeInProcess()
+    {
+        this.busy = true;
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,22 +1,19 @@
 package com.develogical.camera;
 
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.mockito.Mockito.*;
+import org.mockito.ArgumentCaptor;
 
 public class CameraTest {
+
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
         Sensor sensor = mock(Sensor.class);//This is actually a mock, implementing the interface
 
-        Camera camera = new Camera(sensor, mock(MemoryCard.class), mock(WriteCompleteListener.class));//Instancing an actual Camera, with our test constructor, to insert the mock into the object
+        Camera camera = new Camera(sensor, mock(MemoryCard.class));//Instancing an actual Camera, with our test constructor, to insert the mock into the object
 
         camera.powerOn();//Call my method
         verify(sensor).powerUp();//Expect the sensor to be powered up
@@ -27,7 +24,7 @@ public class CameraTest {
     public void switchingOffTheCameraPowersDownTheSensor() {
         Sensor sensor = mock(Sensor.class);//This is actually a mock, implementing the interface
 
-        Camera camera = new Camera(sensor, mock(MemoryCard.class), mock(WriteCompleteListener.class));//Instancing an actual Camera, with our test constructor, to insert the mock into the object
+        Camera camera = new Camera(sensor, mock(MemoryCard.class));//Instancing an actual Camera, with our test constructor, to insert the mock into the object
 
         camera.powerOff();//Call my method
         verify(sensor).powerDown();//Expect the sensor to be powered off
@@ -37,7 +34,7 @@ public class CameraTest {
     public void pressShutterCameraOff(){
         Sensor sensor = mock(Sensor.class);//This is actually a mock, implementing the interface
 
-        Camera camera = new Camera(sensor, mock(MemoryCard.class), mock(WriteCompleteListener.class));//Instancing an actual Camera, with our test constructor, to insert the mock into the object
+        Camera camera = new Camera(sensor, mock(MemoryCard.class));//Instancing an actual Camera, with our test constructor, to insert the mock into the object
 
         camera.pressShutter();
         verifyZeroInteractions(sensor);
@@ -48,16 +45,39 @@ public class CameraTest {
     public void pressShutterCameraOn(){
         Sensor sensor = mock(Sensor.class);//This is actually a mock, implementing the interface
         MemoryCard mc = mock(MemoryCard.class);//This is actually a mock, implementing the interface
-        WriteCompleteListener wcl = mock(WriteCompleteListener.class);
         byte[] myFakeData = new byte[1];
         when(sensor.readData()).thenReturn(myFakeData);
 
-        Camera camera = new Camera(sensor, mc, wcl);//Instancing an actual Camera, with our test constructor, to insert the mock into the object
+        Camera camera = new Camera(sensor, mc);//Instancing an actual Camera, with our test constructor, to insert the mock into the object
         camera.powerOn();
         camera.pressShutter();
         verify(mc, times(1)).write(eq(myFakeData), any());
 
+    }
+
+    @Test
+    public void powerDownWhileWriting(){
+        Sensor sensor = mock(Sensor.class);//This is actually a mock, implementing the interface
+        MemoryCard mc = mock(MemoryCard.class);//This is actually a mock, implementing the interface
+
+        Camera camera = new Camera(sensor, mc);//Instancing an actual Camera, with our test constructor, to insert the mock into the object
 
 
+        camera.powerOn();
+        camera.pressShutter();
+
+        ArgumentCaptor<WriteCompleteListener> argumentCaptor = ArgumentCaptor.forClass(WriteCompleteListener.class);
+        verify(mc).write(any(), argumentCaptor.capture());
+
+
+        camera.powerOff();
+        verify(sensor, times(0)).powerDown();
+
+        argumentCaptor.getValue().writeComplete();
+
+        camera.powerOff();
+        verify(sensor, times(1)).powerDown();
     }
 }
+
+
